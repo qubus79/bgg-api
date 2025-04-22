ee7e68e4026962b96f80f1bc8f2e07109b642457
Pawel Nocznicki <sales@nocznicki.pl>
2025-04-22 16:30:14 +0200

    bool

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.sql import func
 from app.database import Base
